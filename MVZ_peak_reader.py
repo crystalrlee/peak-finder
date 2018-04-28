92ebec28fad5e857dd4826b58549f0b9e41dde80
Crystal Lee <crystalrosemayer@gmail.com>
2018-04-27 23:06:20 -0700

    Got tired, need to deal with sizes and heights being blank, convert to floats

--- MVZ_peak_reader.py
+++ MVZ_peak_reader.py
@@ -17,17 +17,17 @@ def populate_dict():
     # Opens & reads a tab delineated .txt file
     data = open("/home/crystal/Education/bio_work/python_project_MVZ/M3 export all.txt", "r")
     reader = csv.reader(data, delimiter='\t')
-    # Makes a dictionary where each sample is a key, with nested dictionaries for each row in file
+    next(reader, None) # skips the header line
     for row in reader:
+        # Makes a dictionary where each sample is a key
         sample_name = row[1]
         if sample_name not in peak_data:
             peak_data[sample_name] = []
-
         peak_data[sample_name].append({
             'quality': row[7],
             'dye': row[11],
-            'size': row[13],
-            'height': row[14]
+            'size': float(row[13]),
+            'height': float(row[14])
         })
     return peak_data
 
@@ -43,6 +43,15 @@ def check_quality(peak_data):
     for fail in failed_samples:
         del peak_data[fail]
     return peak_data
+
+# Looks for important peaks
+def look_for_peaks(filtered_data):
+    # Checks for M3 peaks
+    M3 = []
+    for sample in filtered_data:
+        for fragment in filtered_data[sample]:
+            if fragment['dye'].startswith('B') and 150 < fragment['size'] < 154:
+                pass
 
 def main():
     peak_data = populate_dict()
